d53202cfdd46e025862415f8e861fcc653a289c4
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-09 18:08:18 +0300

    place an intent to take us to a new activity

--- app/src/main/java/com/example/comicy/MainActivity.java
+++ app/src/main/java/com/example/comicy/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.comicy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +22,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Comics!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, ComicsListActivity.class);
+                startActivity(intent);
             }
         });
     }
-    
+
     }
 
