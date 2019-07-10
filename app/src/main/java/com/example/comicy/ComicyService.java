bc7fe092c7448c88c06d1c61512a1df176b6e0b4
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-10 19:44:00 +0300

    parse JSON data from the API's response to my comicy constructor

--- app/src/main/java/com/example/comicy/ComicyService.java
+++ app/src/main/java/com/example/comicy/ComicyService.java
@@ -1,15 +1,23 @@
 package com.example.comicy;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class ComicyService {
 
-    public  static  void findComics(Callback callback) {
+    public static void findComics(Callback callback) {
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .build();
@@ -25,4 +33,40 @@ public class ComicyService {
         Call call = client.newCall(request);
         call.enqueue(callback);
     }
+
+    public ArrayList<Comicy> processResults(Response response) {
+        ArrayList<Comicy> comics = new ArrayList<>();
+        try {
+            String jsonData = response.body().string();
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray resultsJSON = jsonObject.getJSONArray("results");
+            if (response.isSuccessful()) {
+                for (int i = 0; i < resultsJSON.length(); i++) {
+                    JSONObject comicyJSON = resultsJSON.getJSONObject(i);
+                    String id = comicyJSON.getString("id");
+                    String title = comicyJSON.getString("title");
+                    String issuenumber = comicyJSON.getString("issueNumber");
+                    String description = comicyJSON.getString("description");
+                    String format = comicyJSON.getString("format");
+                    String modified = comicyJSON.getString("modified");
+                    String thumbnail = comicyJSON.getString("thumbnail");
+                    String pagecount = comicyJSON.getString("pageCount");
+
+
+                    Comicy comicy = new Comicy(id, title, issuenumber,
+                            description, format, modified, thumbnail, pagecount);
+                    comics.add(comicy);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return comics;
+
+    }
 }
+
+
