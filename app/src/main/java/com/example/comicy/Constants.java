06fca1ceea3ca278d1f8acf50d0ed04e009fff06
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-10 17:28:57 +0300

    include all constant values from my api into my constants.java

--- app/src/main/java/com/example/comicy/Constants.java
+++ app/src/main/java/com/example/comicy/Constants.java
@@ -2,4 +2,7 @@ package com.example.comicy;
 
 public class Constants {
     public static final String COMICY_TOKEN = BuildConfig.COMICY_TOKEN;
+    public static final String COMICY_BASE_URL = "https://gateway.marvel.com:443/v1/public/comics";
+    public static final String COMICY_APIKEY = "64e5a703987bdaa2d584a3672227d32c";
+    public static final String COMICY_TS = "1";
 }
