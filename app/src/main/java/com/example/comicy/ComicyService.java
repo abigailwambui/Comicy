28166ad8d55422e8bb7c3e29ff73b299f3d3df42
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-10 17:58:03 +0300

    craete a new okhttpClient to create and send our requests

--- app/src/main/java/com/example/comicy/ComicyService.java
+++ app/src/main/java/com/example/comicy/ComicyService.java
@@ -1,13 +1,28 @@
 package com.example.comicy;
 
 
+import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class ComicyService {
 
-    public  static  void findComics(Callback callbakc) {
+    public  static  void findComics(Callback callback) {
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.COMICY_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.COMICY_APIKEY_PARAMETER, Constants.COMICY_APIKEY);
+        urlBuilder.addQueryParameter(Constants.COMICY_TS_PARAMETER, Constants.COMICY_TS);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 }
--- app/src/main/java/com/example/comicy/Constants.java
+++ app/src/main/java/com/example/comicy/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
     public static final String COMICY_TOKEN = BuildConfig.COMICY_TOKEN;
     public static final String COMICY_BASE_URL = "https://gateway.marvel.com:443/v1/public/comics";
     public static final String COMICY_APIKEY = "64e5a703987bdaa2d584a3672227d32c";
+    public static final String COMICY_APIKEY_PARAMETER = "apikey";
     public static final String COMICY_TS = "1";
+    public static final String COMICY_TS_PARAMETER = "ts";
 }
