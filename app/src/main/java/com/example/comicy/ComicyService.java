c675175744df91d984a184f2a65abfa304efda54
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-10 17:46:47 +0300

    create a new okhttpclient to create and send request in service class

--- app/src/main/java/com/example/comicy/ComicyService.java
+++ app/src/main/java/com/example/comicy/ComicyService.java
@@ -1,10 +1,13 @@
 package com.example.comicy;
 
 
+import okhttp3.Callback;
 
 public class ComicyService {
 
     public  static  void findComics(Callback callbakc) {
 
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
     }
 }
