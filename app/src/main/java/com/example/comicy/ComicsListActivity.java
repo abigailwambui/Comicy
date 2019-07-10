79dde7f6dd581af8c086d14bcaf7d7aa0496154b
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-10 19:10:10 +0300

    create a comicy model to create objects containing necessary data for each comic

--- app/src/main/java/com/example/comicy/ComicsListActivity.java
+++ app/src/main/java/com/example/comicy/ComicsListActivity.java
@@ -1,20 +1,27 @@
 package com.example.comicy;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.io.IOException;
 
+import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Response;
 
 public class ComicsListActivity extends AppCompatActivity {
+
+    public static final String TAG = ComicsListActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comics_list);
+        getComics();
     }
 
     private void getComics() {
---
+++ app/src/main/java/com/example/comicy/Comicy.java
@@ -0,0 +1,59 @@
+package com.example.comicy;
+
+public class Comicy {
+    private String mId;
+    private String mTitle;
+    private String mIssueNumber;
+    private String mDescription;
+    private String mFormat;
+    private String mModified;
+    private String mThumbnail;
+    private String mPagecount;
+
+
+    public Comicy(String id, String title, String issueNumber,
+                  String description, String format, String modified,
+                  String thumbnail, String pagecount) {
+        this.mId = id;
+        this.mTitle = title;
+        this.mIssueNumber = issueNumber;
+        this.mDescription = description;
+        this.mFormat = format;
+        this.mModified = modified;
+        this.mThumbnail = thumbnail;
+        this.mPagecount = pagecount;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getIssueNumber() {
+        return mIssueNumber;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getFormat() {
+        return mFormat;
+    }
+
+    public String getModified() {
+        return mModified;
+    }
+
+    public String getThumbnail() {
+        return mThumbnail;
+    }
+
+    public String getPagecount() {
+        return mPagecount;
+    }
+
+}
