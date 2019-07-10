c6165300a35a908b6bc2a0313b8f35bb0540a67f
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-10 18:03:26 +0300

    create a callback method inside of ComicyListActivity

--- app/src/main/java/com/example/comicy/ComicsListActivity.java
+++ app/src/main/java/com/example/comicy/ComicsListActivity.java
@@ -3,6 +3,9 @@ package com.example.comicy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import okhttp3.Callback;
 
 public class ComicsListActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class ComicsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comics_list);
+    }
+
+    private void getComics() {
+        final ComicyService comicyService = new ComicyService();
+        comicyService.findComics(new Callback() {
+        });
     }
 }
