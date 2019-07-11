34d882bb064646c146aa30dcc99f9c71eddc2476
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-11 16:22:51 +0300

    create a fragment

---
+++ app/src/main/java/com/example/comicy/ui/ComicyDetailFragment.java
@@ -0,0 +1,30 @@
+package com.example.comicy.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.comicy.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ComicyDetailFragment extends Fragment {
+
+
+    public ComicyDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comicy_detail, container, false);
+    }
+
+}
