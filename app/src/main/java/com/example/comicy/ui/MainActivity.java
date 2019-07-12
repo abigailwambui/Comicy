85a9aea86fdbcc4be84932bafb6af911b12cfa04
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-12 08:54:11 +0300

    create a login activity

--- app/src/main/java/com/example/comicy/ui/MainActivity.java
+++ app/src/main/java/com/example/comicy/ui/MainActivity.java
@@ -5,31 +5,74 @@ package com.example.comicy.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.comicy.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mFindComicsButton;
+    @BindView (R.id.findComicsButton) Button mFindComicsButton;
+    @BindView(R.id.savedComicsButton) Button mSavedComicsButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mFindComicsButton = (Button) findViewById(R.id.findComicsButton);
-        mFindComicsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Comics!", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(MainActivity.this, ComicsListActivity.class);
-                startActivity(intent);
-            }
-        });
+        mFindComicsButton.setOnClickListener(this);
+        mSavedComicsButton.setOnClickListener(this);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFindComicsButton) {
+            Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this, ComicsListActivity.class);
+            startActivity(intent);
+        }
+
+        if (v == mSavedComicsButton) {
+            Intent intent = new Intent(MainActivity.this, SavedArtListActivity.class);
+            startActivity(intent);
+        }
+
+    }
+}
 
